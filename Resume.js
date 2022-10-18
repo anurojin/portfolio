811343ddefc74824b8c6b15b1f59f5fa07032702
anurojin <97700648+anurojin@users.noreply.github.com>
2022-10-18 11:55:06 +0100

    Update Resume.js

--- Resume.js
+++ Resume.js
@@ -1,6 +1,6 @@
 import { saveAs } from "file-saver";
 const Resume = () => {
-saveAs("https://drive.google.com/file/d/1WS_UC7SLucHAsacgQgwqdq6XqbC2JAiC/view?usp=sharing","Anu Mariam Skariah -RESUME")
+saveAs("https://drive.google.com/file/d/1A0305efVylQe3Il3zBn8m7OgsuCtA4av/view?usp=sharing","Anu Mariam Skariah -RESUME")
     return ( {Resume} );
 }
  
